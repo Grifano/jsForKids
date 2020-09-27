8128eac69cfdab724c6512560815552fccb49ca7
Grifano <sorlenk@gmail.com>
2020-09-27 21:31:36 +0200

    get numbers by one command

---
+++ js/crazy-object.js
@@ -0,0 +1,6 @@
+var myCrazyObject = {
+	"name": "stupid object",
+	"some array": [7, 9, { purpose: "anythins", numbers: 123 }, 3.3],
+	"random animals": "Banan shark",
+};
+console.log(myCrazyObject["some array"][2].numbers);
